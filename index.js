66e36bc9a328a5659eef8e3697a07b4bdeab7212
farque65 <fahim.farque@gmail.com>
2019-05-03 22:38:39 -0400

    token.json updates

--- index.js
+++ index.js
@@ -96,17 +96,19 @@ function sendMessage(recipientId, message) {
 
 
 function authorize(credentials, callback) {
-    console.log("cred check", credentials);
+
   const {client_secret, client_id, redirect_uris} = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]);
 
+return getNewToken(oAuth2Client, callback);
+
   // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
+//   fs.readFile(TOKEN_PATH, (err, token) => {
+//     if (err) return getNewToken(oAuth2Client, callback);
+//     oAuth2Client.setCredentials(JSON.parse(token));
+//     callback(oAuth2Client);
+//   });
 }
 
 function getNewToken(oAuth2Client, callback) {
